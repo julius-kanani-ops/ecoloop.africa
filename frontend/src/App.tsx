2e51600d130365f8c5e28024df5aa0b304ab7043
Julius Kanani <juliuskanani.sofwarengineer@gmail.com>
2025-10-20 18:05:39 +0300

    chore: setup basic EcoLoop Africa project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        Welcome to EcoLoop Africa!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
